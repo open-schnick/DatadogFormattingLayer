9f0075be565bb52218431c3c83ddc28f54c161f4
open-schnick <dev2@filefighter.de>
2023-09-18 18:10:58 +0200

    chore: use clippy instead of cargo-lints, add more lints

--- src/lib.rs
+++ src/lib.rs
@@ -92,11 +92,37 @@
 //! {"timestamp":"2023-06-21T10:36:50.363384118+00:00","level":"INFO","message":"Bla fasel user=Jack ola=salve value=Fasel hello=world","target":"otel","dd.trace_id":0,"dd.span_id":10201226522570980512}
 //! ```
 
-#![deny(rust_2018_idioms)]
-#![deny(clippy::correctness)]
-#![deny(clippy::perf)]
-#![deny(clippy::all)]
-#![deny(clippy::pedantic)]
+#![deny(
+    rust_2018_idioms,
+    unused_must_use,
+    clippy::nursery,
+    clippy::pedantic,
+    clippy::perf,
+    clippy::cargo,
+    clippy::correctness,
+    clippy::dbg_macro,
+    clippy::else_if_without_else,
+    clippy::empty_drop,
+    clippy::empty_structs_with_brackets,
+    clippy::expect_used,
+    clippy::if_then_some_else_none,
+    clippy::indexing_slicing,
+    clippy::integer_division,
+    clippy::multiple_inherent_impl,
+    clippy::panic,
+    clippy::print_stderr,
+    clippy::print_stdout,
+    clippy::same_name_method,
+    clippy::string_to_string,
+    clippy::todo,
+    clippy::try_err,
+    clippy::unimplemented,
+    clippy::unnecessary_self_imports,
+    clippy::unreachable,
+    clippy::unwrap_used,
+    clippy::wildcard_enum_match_arm,
+    missing_docs
+)]
 #![allow(clippy::module_name_repetitions)]
 
 mod datadog_ids;
